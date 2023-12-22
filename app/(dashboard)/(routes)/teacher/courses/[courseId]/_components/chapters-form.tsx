ebacd43a8688540d1ef8c7db57acaf8e0cb24af2
Olah Israel <olahisrael07@gmail.com>
2023-12-22 09:01:45 +0100

    Added Drag and Drop feature to Chapter section

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -13,7 +13,7 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -63,8 +63,28 @@ export const ChaptersForm = ({
         
     }
 
+    const onReorder = async (updateData: { id: string; position: number }[]) => {
+        try {
+            setIsUpdating(true);
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            });
+            toast.success("Chapters reordered");
+            router.refresh();
+        } catch {
+            toast.error("something went wrong");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
     return (
         <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course chapter
                 <Button variant="ghost" onClick={toggleCreating}>
@@ -121,7 +141,7 @@ export const ChaptersForm = ({
                     
                     <ChaptersList
                         onEdit={()=> {}}
-                        onReorder={()=> {}}
+                        onReorder={onReorder}
                         items={initialData.chapters || []}
                     />
                 </div>
