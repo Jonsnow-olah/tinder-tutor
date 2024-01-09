8a62201c26867148958bd495199db913a7cd90cb
Olah Israel <olahisrael07@gmail.com>
2024-01-09 18:01:18 +0100

    Added data table feature to courses page

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,25 +1,89 @@
 "use client"
 
+import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
 
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
+import { Button } from "@/components/ui/button"
 
-export const columns: ColumnDef<Payment>[] = [
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link"
+
+
+export const columns: ColumnDef<Course>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
   },
+
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
   },
+
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "isPublished",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
   },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-4 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4"/>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2"/>
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
 ]
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,23 +1,30 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-async function getData(): Promise<any[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      // ...
-    ]
-  }
+const CoursesPage = async () => {
+    
+    const { userId } = auth();
 
-const CoursesPage = () => {
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
     return (
-        <DataTable columns={columns} data={data} />
+        <div className="p-6">
+            <DataTable columns={columns} data={courses} />
+        </div>
       );
 }
  
