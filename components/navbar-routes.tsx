a7836b542020d13e43c2be39e92d525627d5fec3
Olah Israel <olahisrael07@gmail.com>
2024-01-16 13:05:17 +0100

    Added toggle to search course category page

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -12,26 +12,34 @@ export const  NavbarRoutes = () => {
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search";
 
     return (
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                    <Button size="sm" variant="ghost">
-                        <LogOut className="h-4 w-4 mr-2" />
-                        Exit
-                    </Button>
-                </Link>
-            ): (
-                <Link href="/teacher/courses">
-                    <Button size="sm" variant="ghost">
-                        Teacher Mode
-                    </Button>
-                </Link>
-            )}
-            <UserButton
-                afterSignOutUrl="/" //If you don't add this, and you click on sign out, it will redirect you to the clerk website instead of logging you out
-            />
-        </div>
+        <>
+        {isSearchPage && (
+            <div className="hidden md:block">
+                <SearchInput />
+            </div>
+        )}
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="ghost">
+                            <LogOut className="h-4 w-4 mr-2" />
+                            Exit
+                        </Button>
+                    </Link>
+                ): (
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost">
+                            Teacher Mode
+                        </Button>
+                    </Link>
+                )}
+                <UserButton
+                    afterSignOutUrl="/" //If you don't add this, and you click on sign out, it will redirect you to the clerk website instead of logging you out
+                />
+            </div>
+        </>
     )
 }
