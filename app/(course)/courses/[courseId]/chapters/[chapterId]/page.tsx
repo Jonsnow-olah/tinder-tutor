f1bfeddc2a880bcaacf0a9a2da99e9950379a6a5
Olah Israel <olahisrael07@gmail.com>
2024-02-04 08:49:34 +0100

    chapter page implementation

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,8 +1,21 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async({
+    params
+}: {
+    params: { courseId: string; chapterId: string;}
+}) => {
+
+    const { userId } = auth();
+
+    if(!userId) {
+        return redirect("/");
+    }
 
     return ( 
         <div>
-            ChapterId
+            Chapter Id
         </div>
      );
 }
