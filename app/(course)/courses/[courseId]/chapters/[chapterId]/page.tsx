8fd69378618d0383528f564e9deaccc79ecd274a
olah <olahisrael07@gmail.com>
2024-02-17 20:42:41 +0100

    course-progress-button:Implemented progress butto

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -10,6 +10,7 @@ import { Preview } from "@/components/preview";
 import { VideoPlayer } from "./_components/video-player";
 
 import { CourseEnrollButton } from "../../_components/course-enroll-button";
+import { CourseProgressButton } from "../../_components/course-progress-button";
 
 const ChapterIdPage = async ({
   params
@@ -76,9 +77,12 @@ const ChapterIdPage = async ({
               {chapter.title}
             </h2>
             {purchase ? (
-              <div>
-                
-              </div>
+              <CourseProgressButton 
+                  chapterId={params.chapterId}
+                  courseId={params.courseId}
+                  nextChapterId={nextChapter?.id}
+                  isCompleted={!!userProgress?.isCompleted}
+              />
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
